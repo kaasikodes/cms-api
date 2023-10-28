5476873b8da2a0b44fcf75efd7abd3ce4365b235
kaasikodes <thisbroisfresh@gmail.com>
2023-10-28 06:15:06 +0100

    chore(routes): defines auth routes

---
+++ src/routes/v1/auth/index.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import authController from "../../../controllers/auth";
+import authMiddleware from "../../../middleware/authHandler";
+import validator from "../../../middleware/validator";
+import authSchemas from "../../../validation/auth";
+
+const authRouter = Router();
+const { signup, login, resetPassword, forgotPassword, logout } = authController;
+const { verifyResetPasswordToken, verifyAuthToken } = authMiddleware;
+const { signupSchema, loginSchema, resetPasswordSchema, forgotPasswordSchema } =
+  authSchemas;
+
+authRouter.post("/signup", validator(signupSchema), signup);
+authRouter.post("/login", validator(loginSchema), login);
+authRouter.post(
+  "/forgot-password",
+  validator(forgotPasswordSchema),
+  forgotPassword
+);
+authRouter.post(
+  "/reset-password",
+  validator(resetPasswordSchema),
+  verifyResetPasswordToken,
+  resetPassword
+);
+authRouter.post("/logout", verifyAuthToken, logout);
+
+export default authRouter;
